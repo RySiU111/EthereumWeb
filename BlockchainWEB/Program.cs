1cb6fbf4d6c59bf47eb60a0660ec1e4101ff9e83
DeathArmy <46004028+DeathArmy@users.noreply.github.com>
2022-01-21 21:56:54 +0100

    BlockchainWEB implementation

--- BlockchainWEB/Program.cs
+++ BlockchainWEB/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using BlockchainWEB.Services;
 
 namespace BlockchainWEB
 {
@@ -19,8 +18,6 @@ namespace BlockchainWEB
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<IBlockchainService, BlockchainService>();
-                
 
             await builder.Build().RunAsync();
         }
